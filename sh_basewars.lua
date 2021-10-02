9f19047538d86658cd2677211a437ac268116922
Stromic <38598074+Stromic@users.noreply.github.com>
2021-10-02 12:59:32 +0200

    Added level support

--- sh_basewars.lua
+++ sh_basewars.lua
@@ -32,4 +32,73 @@ hook.Add("PostGamemodeLoaded", "sA:LoadBaseWars", function()
             sAdmin.msg(silent and ply or nil, "addmoney_response", ply, amount, targets)
         end
     })
+    
+    sAdmin.addCommand({
+        name = "setlevel",
+        category = "BaseWars",
+        inputs = {{"player", "player_name"}, {"numeric", "amount"}},
+        func = function(ply, args, silent)
+            local targets = sAdmin.getTargets("setlevel", ply, args[1], 1)
+            local level = tonumber(args[2]) or 0
+    
+            for k,v in ipairs(targets) do
+                v:SetLevel(level)
+            end
+    
+            sAdmin.msg(silent and ply or nil, "setlevel_response", ply, targets, level)
+        end
+    })
+    
+    sAdmin.addCommand({
+        name = "addlevel",
+        category = "BaseWars",
+        inputs = {{"player", "player_name"}, {"numeric", "amount"}},
+        func = function(ply, args, silent)
+            local targets = sAdmin.getTargets("setlevel", ply, args[1], 1)
+            local level = tonumber(args[2]) or 0
+    
+            for k,v in ipairs(targets) do
+                v:AddLevel(level)
+            end
+    
+            sAdmin.msg(silent and ply or nil, "addlevel_response", ply, level, targets)
+        end
+    })
+    
+    sAdmin.addCommand({
+        name = "setxp",
+        category = "BaseWars",
+        inputs = {{"player", "player_name"}, {"numeric", "amount"}},
+        func = function(ply, args, silent)
+            local targets = sAdmin.getTargets("setxp", ply, args[1], 1)
+            local xp = tonumber(args[2]) or 0
+    
+            for k,v in ipairs(targets) do
+                v:SetXP(xp)
+            end
+    
+            sAdmin.msg(silent and ply or nil, "setxp_response", ply, targets, xp)
+        end
+    })
+    
+    sAdmin.addCommand({
+        name = "addxp",
+        category = "BaseWars",
+        inputs = {{"player", "player_name"}, {"numeric", "amount"}},
+        func = function(ply, args, silent)
+            local targets = sAdmin.getTargets("addxp", ply, args[1], 1)
+            local xp = tonumber(args[2]) or 0
+    
+            for k,v in ipairs(targets) do
+                v:AddXP(xp)
+            end
+    
+            sAdmin.msg(silent and ply or nil, "addxp_response", ply, xp, targets)
+        end
+    })
+
+    slib.setLang("sadmin", "en", "setlevel_response", "%s set %s's level to %s.")
+    slib.setLang("sadmin", "en", "addlevel_response", "%s added %s levels to %s.")
+    slib.setLang("sadmin", "en", "setxp_response", "%s set %s's XP to %s.")
+    slib.setLang("sadmin", "en", "addxp_response", "%s added %s XP to %s.")
 end)
