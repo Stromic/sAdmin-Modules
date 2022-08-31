9902a312718468e702184b965970a7a7a3a02a54
Exehad <exehad701@gmail.com>
2022-08-31 12:11:04 -0400

    finished the base admin mode

--- sh_adminmode.lua
+++ sh_adminmode.lua
@@ -9,7 +9,8 @@ sAdmin.addCommand({
             if !v:GetNWString( "is_admined", false) then
                 local sid64 = v:SteamID64()
                 local model = v:GetModel()
-                v:SetNWString( "model", model )
+                v:SetNWString( "bfa_model", model )
+                v:SetModel( "models/player/combine_super_soldier.mdl" )
                 v:SetNWString( "is_admined", true )
                 sAdmin.godded[sid64] = true
             else
@@ -30,9 +31,16 @@ sAdmin.addCommand({
         local targets = sAdmin.getTargets("unadmin", ply, args[1], 1)
         
         for k,v in ipairs(targets) do
-            local sid64 = v:SteamID64()
-            sAdmin.godded[sid64] = nil
-            sAdmin.adminHandle(v)
+            if v:GetNWString( "is_admined", false) then
+                local sid64 = v:SteamID64()
+                local model = v:GetNWString( "bfa_model" )
+                v:SetModel( model )
+                v:SetNWString( "is_admined", false )
+                sAdmin.godded[sid64] = nil
+            else
+                sAdmin.msg(ply, "no_adminmode_response", targets)
+                return
+            end 
         end
 
         sAdmin.msg(silent and ply or nil, "adminmode_response", ply, targets)
@@ -42,4 +50,5 @@ sAdmin.addCommand({
 
 slib.setLang("sadmin", "en", "adminmode_response", "%s put %s into adminmode")
 slib.setLang("sadmin", "en", "in_adminmode_response", "%s is already in adminmode!")
+slib.setLang("sadmin", "en", "no_adminmode_response", "%s is not in adminmode!")
 slib.setLang("sadmin", "en", "unadminmode_response", "%s took %s out of adminmode")
